f43b1e216cbe982775a3564ef803d6b408033067
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-08-24 17:12:49 +0900

    add UserSchema

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+
+//user schema 생성
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+    
+});
+
+const User = mongoose.model('user', UserSchema)
+module.exports = { User }
