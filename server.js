a06e997a655144bbf35287ecd821863729981811
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-08-24 16:17:48 +0900

    connect database

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 //express로 app만들기
 const PORT = process.env.PORT || 5000;
 //default port 5000으로 설정
+const connectDB = require('./config/db');
+
+//database 연결하기
+connectDB();
+
 
 app.get('/',(req,res)=> res.send('server running'))
 
