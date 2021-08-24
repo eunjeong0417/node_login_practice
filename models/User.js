3e390ca780bcb42d2ad414d5bdfd0c01f4f5d598
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-08-24 21:57:24 +0900

    user register

--- models/User.js
+++ models/User.js
@@ -27,5 +27,4 @@ const UserSchema = mongoose.Schema({
     
 });
 
-const User = mongoose.model('user', UserSchema)
-module.exports = { User }
+module.exports = User = mongoose.model('user', UserSchema)
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,26 +1,65 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
+//schema가져오기
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
 
 
 
 //register user
 //post request
-router.post('/', [
+router.post('/', 
     check('name', '이름을 입력하세요')
         .not()
-        .isEmpty(),
+        .isEmpty(),      
     check('email', '이메일을 확인하세요')
         .isEmail(),
     check('password', '비밀번호는 최소 6자리 이상입니다')
-        .isLength({ min: 6 })
-],
-    (req, res) => {
+        .isLength({ min: 6 }),
+ async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         } 
-    res.send('user page')
+    
+     const { name, email, password } = req.body;
+     
+     try {
+         let user = await User.findOne({ email });
+
+         if (user) {
+            return res.status(400).json({ errors: [{ msg: '다른 사용자가 사용하는 이메일 입니다' }] });
+         }
+
+         const avatar = gravatar.url(email, {
+             s: '200',
+             r: 'pg',
+             d: 'mm'
+         });
+
+
+             
+
+         user = new User({
+             name,
+             email,
+             avatar,
+             password
+         });
+
+         const salt = await bcrypt.genSalt(10);
+         user.password = await bcrypt.hash(password, salt);
+         await user.save();
+         
+        res.send('user registered');
+        
+     } catch(err) {
+         console.error(err.message);
+         res.status(500).send('Server error');
+    }
 })
 
 module.exports = router;
